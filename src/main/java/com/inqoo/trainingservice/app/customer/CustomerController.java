fb58733ef32a78b89d5ee020d65929a1296e2876
PES34 <4headx@gmail.com>
2021-08-01 12:28:37 +0200

    customer controller ready

---
+++ src/main/java/com/inqoo/trainingservice/app/customer/CustomerController.java
@@ -0,0 +1,23 @@
+package com.inqoo.trainingservice.app.customer;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CustomerController {
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+    @GetMapping
+    private List<Customer> getList(){ return customerService.getAllCustomerList();};
+
+    @PostMapping
+    private Customer saveNew(@RequestBody Customer customer){
+        return  customerService.saveNewCustomer(customer);
+    }
+}
